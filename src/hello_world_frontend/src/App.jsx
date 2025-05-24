ff520a624ab5a012ba6ce342d328088eed681966
Rafael Santin <rasantin@gmail.com>
2025-05-23 21:09:34 -0300

    Minhas modificações: atualizações no backend, frontend e adição de imagem

--- src/hello_world_frontend/src/App.jsx
+++ src/hello_world_frontend/src/App.jsx
@@ -8,7 +8,8 @@ function App() {
     event.preventDefault();
     const name = event.target.elements.name.value;
     hello_world_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
+      const lines = greeting.split('\n');
+      setGreeting(lines.map((line,i) => <div key ={i}>{line}</div>));
     });
     return false;
   }
@@ -17,11 +18,12 @@ function App() {
     <main>
       <img src="/logo2.svg" alt="DFINITY logo" />
       <br />
+      <img src="/UFVJM.png" alt="UFVJM logo" />
       <br />
       <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <label htmlFor="name">Digite o seu nome: &nbsp;</label>
         <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
+        <button type="submit">Clique aqui!</button>
       </form>
       <section id="greeting">{greeting}</section>
     </main>
